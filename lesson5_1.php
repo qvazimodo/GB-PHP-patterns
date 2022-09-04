596d3de070c0ed58bf82c405d0a087117868d27e
art <qvazimodo@mail.ru>
2022-09-04 18:45:24 +0300

    дз урок 5

--- lesson5_1.php
+++ lesson5_1.php
@@ -1,1 +1,48 @@
 <?php
+interface ISender{
+    public function send();
+}
+
+abstract class SenderDecorator implements ISender{
+    protected ISender $sender;
+
+    /**
+     * SenderDecorator constructor.
+     * @param ISender $sender
+     */
+    public function __construct(ISender $sender)
+    {
+        $this->sender = $sender;
+    }
+
+
+}
+
+class SenderEmail extends SenderDecorator{
+
+    public function send()
+    {
+        // TODO: Implement send() method.
+    }
+}
+
+class SenderSms extends SenderDecorator{
+
+    public function send()
+    {
+        // TODO: Implement send() method.
+    }
+}
+
+class SenderCN extends SenderDecorator{
+
+    public function send()
+    {
+        // TODO: Implement send() method.
+    }
+}
+
+$sender = new SenderCN(new SenderEmail(new SenderSms()));
+
+$sender->send();
+
