498cdb5926f38e43d508d7c4c55d32c909f73713
art <qvazimodo@mail.ru>
2022-09-08 19:06:57 +0300

    дз6

--- 6_1.php
+++ 6_1.php
@@ -1,1 +1,80 @@
 <?php
+// Реализация паттерна "Наблюдатель"
+abstract class Subject {
+    abstract public function attach(Observer $observer);
+
+    abstract public function detach(Observer $observer);
+
+    abstract protected function notify();
+}
+
+class VacancyRegisterManager extends Subject {
+    /** @var Observer[] */
+    protected array $observers = [];
+
+    public function attach(Observer $observer)
+    {
+        $this->observers[] = $observer;
+    }
+
+    public function detach(Observer $observer)
+    {
+        foreach($this->observers as $key => $item){
+            if ($item == $observer ){
+                unset($this->observers[$key]);
+            }
+        }
+
+    }
+
+    protected function notify(){
+        foreach ($this->observers as $observer) {
+            $observer->handle($this);
+        }
+    }
+
+    public function vacancyRegister()
+    {
+        //
+        $this->notify();
+        //
+        return true;
+    }
+}
+
+interface Observer {
+    public function handle($subject);
+}
+
+class User implements Observer {
+    protected string $userName;
+    protected string $userMail;
+    protected int $userExp;
+
+    /**
+     * User constructor.
+     * @param string $userName
+     * @param string $userMail
+     * @param $userExp
+     */
+    public function __construct(string $userName, string $userMail, $userExp)
+    {
+        $this->userName = $userName;
+        $this->userMail = $userMail;
+        $this->userExp = $userExp;
+    }
+
+    public function handle($subject)
+    {
+        mail($this->userMail,"",$subject);
+    }
+
+}
+
+$vacancyManager = new VacancyRegisterManager();
+
+$vacancyManager->attach(new User("Иван","123@erfg.ru",));
+
+
+
+
--- 6_3.php
+++ 6_3.php
@@ -1,1 +1,107 @@
 <?php
+//Реализация паттерна "Команда"
+interface CommandInterface {
+    public function execute();
+
+    public function undo();
+
+}
+
+class CopyCommand implements CommandInterface{
+    private string $text;
+
+    /**
+     * CopyCommand constructor.
+     * @param string $text
+     */
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+
+    public function execute($text)
+    {
+        // логика команды "копировать"
+    }
+
+    public function undo($text)
+    {
+        // логика отмены команды "копировать"
+    }
+
+    protected function log()
+    {
+        // логика логгирования отмены команды "копировать"
+    }
+}
+
+class CutCommand implements CommandInterface{
+    private string $text;
+
+    /**
+     * CutCommand constructor.
+     * @param string $text
+     */
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+
+    public function execute()
+    {
+        // логика команды "вырезать"
+    }
+
+    public function undo()
+    {
+        // логика отмены команды "вырезать"
+    }
+
+    protected function log()
+    {
+        // логика логгирования отмены команды "вырезать"
+    }
+}
+
+class PasteCommand implements CommandInterface{
+    private string $text;
+
+    /**
+     * PasteCommand constructor.
+     * @param string $text
+     */
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+
+    public function execute()
+    {
+        // логика команды "вставить"
+    }
+
+    public function undo()
+    {
+        // логика отмены команды "вставить"
+    }
+
+    protected function log()
+    {
+        // логика логгирования отмены команды "вставить"
+    }
+}
+
+
+class TextManager {
+    public function submit(CommandInterface $command)
+    {
+        $command->execute();
+    }
+
+}
+
+$invoker = new TextManager();
+$invoker->submit(new CutCommand("XSWEDFVC"));
